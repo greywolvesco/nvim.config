a2a5ef12675c1488460c52dc8e51030b9a9e17d9
Pawel Poturalski <paul@greywolves.co>
2024-03-13 19:18:02 +0100

    Update formatting settings and some cleanup

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "catppuccin/nvim",
+    'catppuccin/nvim',
     lazy = false,
-    name = "catppuccin",
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
 }
