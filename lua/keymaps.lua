46c78b839ffa8d02061147d30d1d1150365a641c
Pawel Poturalski <paul@greywolves.co>
2024-03-14 08:53:39 +0100

    Fix typo in keymap desc

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,7 +13,7 @@ map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]ui
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
-map('n', '<left>', '<cmd>echo "Use h to move left!<CR>')
+map('n', '<left>', '<cmd>echo "Use h to move left!"<CR>')
 map('n', '<right>', '<cmd>echo "Use l to move right!"<CR>')
 map('n', '<up>', '<cmd>echo "Use k to move up!"<CR>')
 map('n', '<down>', '<cmd>echo "Use j to move down!"<CR>')
