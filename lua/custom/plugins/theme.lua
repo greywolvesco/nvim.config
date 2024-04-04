6cd16ca800ddbb9b9598667f5d7f07faea7bf296
Pawel Poturalski <paul@greywolves.co>
2024-04-04 13:24:45 +0200

    OLED Theme changes; Temp fix for folding

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,14 +1,33 @@
 return {
   {
+    'nvim-lualine/lualine.nvim',
+    lazy = false,
+    name = 'lualine',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+        },
+      }
+    end,
+  },
+  {
     'catppuccin/nvim',
     lazy = false,
     name = 'catppuccin',
     config = function()
       require('catppuccin').setup {
-        -- configurations
+        color_overrides = {
+          all = {
+            base = '#000000',
+          },
+        },
       }
 
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
